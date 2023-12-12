d3dff6f3941c594bd94bebc4f9fc773bb8dcc7e4
William Gyasi <gyasi.wk@gmail.com>
2023-12-12 12:40:01 +0000

    Adding Footer

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const history__video = document.getElementById("history__video");
 //PLAY VIDEO
 play__button.addEventListener("click", (e) => {
   if (history__video.paused) {
+    console.log("video is paused")
     handlePlayButton(history__video)
   }
 });

e3e9fd68b4126a6d1e6b9baf8223bb437f07e68d
William Gyasi <gyasi.wk@gmail.com>
2023-12-12 01:06:47 +0000

    Play Animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
-});
+
+
+console.log("woeac")
+//PLAY ANIMATION
+const play__button=document.querySelector("#play-button")
+console.log(play__button)

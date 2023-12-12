c8f52f6c546bf6442887b666ccc03e2c9b352f59
William Gyasi <gyasi.wk@gmail.com>
2023-12-12 01:30:03 +0000

    Added Play Button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+const play__button = document.querySelector("#play-button");
+const video__overlay = document.getElementById("video__overlay");
+const history__video = document.getElementById("history__video");
 
+//PLAY VIDEO
+play__button.addEventListener("click", (e) => {
+  if (history__video.paused) {
+    handlePlayButton()
+  }
+});
 
-console.log("woeac")
-//PLAY ANIMATION
-const play__button=document.querySelector("#play-button")
-console.log(play__button)
+function handlePlayButton() {
+  history__video.play();
+  video__overlay.classList.remove("show");
+}
